a3a6ef3af6f86924d69169e0d8d3b0f7ceeb58e4
pencilbsp <pencil.bsp@gmail.com>
2025-06-22 22:04:14 +0700

    update

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,9 +4,36 @@ import { Elysia, t } from 'elysia';
 import { CustomBase64 } from 'abyass/utils';
 
 import { key } from '../utils/redis';
+import { DOMAIN } from '../config';
 
 const MAX_AGE = 86400 / 12;
 const htmlFile = Bun.file('public/core.html');
+
+function stringToHex(str: string) {
+    let hex = '';
+    for (let i = 0; i < str.length; i++) {
+        hex += str.charCodeAt(i).toString(16).padStart(2, '0');
+    }
+    return hex;
+}
+
+const makeUrl = (a: string, prefix?: string) => {
+    try {
+        let urlString = a;
+        if (urlString.startsWith('//')) {
+            urlString = window.location.protocol + urlString;
+        }
+        const url = new URL(urlString);
+        if (prefix) {
+            url.pathname = prefix + url.pathname;
+        }
+        url.searchParams.set('h', stringToHex(url.host));
+        url.host = new URL(DOMAIN).host;
+        return url.toString();
+    } catch {
+        return a;
+    }
+};
 
 const appRoute = new Elysia().get(
     '/',
@@ -20,6 +47,10 @@ const appRoute = new Elysia().get(
                 await abyass.extract();
 
                 const videoObject = abyass.getVideoObject();
+
+                if (videoObject.image) {
+                    videoObject.image = makeUrl(videoObject.image);
+                }
 
                 if (videoObject.ads) {
                     videoObject.ads.pop = [];
